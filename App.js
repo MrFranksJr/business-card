9fe830b763e494b4c9f18a8d3e53bf3f95801d15
MrFranksJr <frankyjr@me.com>
2023-08-22 16:15:53 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+import ProfilePic from "./components/profilePic.js"
+
+export default function App() {
+    return <div>
+            <ProfilePic />
+        </div>
+}
---
+++ components/ProfilePic.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function ProfilePic() {
+    return <img className="profilePic" src='../assets/images/profile.png' alt='' />
+}
